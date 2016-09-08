8d900cf755c207850aa02934ff02c91689b8f6ce
L.Joel <leopard10000@gmail.com>
2016-09-08 21:17:31 +0900

    [メール送信] メール内にEntryページを開くLinkを追加 ※ホスト名は development.rb に設定

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -3,7 +3,7 @@ class CommentMailer < ApplicationMailer
   def new_comment_email(comment, blog)
     @comment = comment
     @blog = blog
-    @url  = "http://localhost:3000/blogs/#{blog.id}/entries/#{blog.entries[0].id}/comments/#{comment.id}"
+    @entry_url  = "http://#{Rails.application.config.action_mailer.default_url_options[:host]}/blogs/#{blog.id}/entries/#{blog.entries[0].id}"
     mail(to: 'admin@example.com', subject: '新しいコメントが投稿されました')
   end
 
